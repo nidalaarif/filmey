04b21764e496c720295a63fa509c30d09b38fc29
nidal aarif <nidal1aarif@gmail.com>
2020-02-03 16:58:42 +0100

    adding omdb API

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -11,6 +11,25 @@ use League\Flysystem\Config;
 
 class MoviesController extends Controller
 {
+    public function updateMovieFields($title,$year){
+        $title = str_replace(' ','+',$title);
+        $url = 'https://www.omdbapi.com/?t='.$title.'&y='.$year.'&apikey=1efc01bf';
+        $flag = false;
+
+        try {
+            $jsonData = file_get_contents($url);
+            $movieObject = json_decode($jsonData, true);
+            if ($movieObject['Response'] == 'True'){
+                $flag = true;
+
+            }else{
+                $flag = false;
+            }
+        } catch (\Exception $e) {
+            $flag = false;
+        }
+        return $flag == true ? $movieObject : null;
+    }
     public  function getCategories($movie_cat){
         $cats = explode(" / ",$movie_cat);
         $genres = [];
@@ -66,35 +85,69 @@ class MoviesController extends Controller
      */
     public function show(Movie $movie)
     {
-        // Get details of the movie
-        $data = [];
-        //$details =  Movie::find($movie);
-        $links = $movie->movie_links()->where('quality_type','!=','screenshot')->get();
-        $screenshots = $movie->movie_links()->where('quality_type','screenshot')->get();
-        $data['links'] = $links;
-        $data['screenshots'] = $screenshots;
-
-        $links = $movie->movie_links()->where('quality_type','!=','screenshot')->get();
-        $screenshots = $movie->movie_links()->where('quality_type','screenshot')->get();
-        $directors = $movie->crew()->where('profession','director')->get();
-        $actors = $movie->crew()->where('profession','!=','director')->get();
-        $data['details'] = $movie;
-        $data['links'] = $links;
-        $data['screenshots'] = $screenshots;
-        $data['directors'] = $directors;
-        $data['actors'] = $actors;
-        $data['genre'] = $this->getCategories($movie->category);
-        // ---------------- //
-        //  Get related movies
-        // TODO: FIX related movies categories
-        $related_movies = Movie::take(7)->orWhere('category','like','%'.$movie->category.'%')
-                                        ->where('id','!=',$movie->id)
-                                        ->where('title','!=',$movie->title)
-                                        ->orderBy('year','DESC')->get();
-        $data['related'] = $related_movies;
-
-
-        return view('movieDetails',compact('data'));
+//        if ($movie->scraped == 0){
+//            $updatedmovie = $this->updateMovieFields($movie->title,$movie->year);
+//            if ($updatedmovie != null){
+//                $updateArray = [
+//                  'rated' => $updatedmovie['Rated'],
+//                  'released' => $updatedmovie['Released'],
+//                  'runtime' => $updatedmovie['Runtime'],
+//                  'genre' => $updatedmovie['Genre'],
+//                  'plot' => $updatedmovie['Plot'],
+//                  'language' => $updatedmovie['Language'],
+//                  'country' => $updatedmovie['Country'],
+//                  'awards' => $updatedmovie['Awards'],
+//                  'rating_imd' => '',
+//                  'rating_rt' => '',
+//                  'rating_m' => '',
+//                  'metascore' => $updatedmovie['Metascore'],
+//                  'imdbVotes' => $updatedmovie['imdbVotes'],
+//                  'imdbID' => $updatedmovie['imdbID'],
+//                  'dvd' => $updatedmovie['DVD'],
+//                  'boxOffice' => $updatedmovie['BoxOffice'],
+//                  'production' => $updatedmovie['Production'],
+//                  'scraped' => 1,
+//                ];
+//                if (count($updatedmovie['Ratings']) > 0){
+//
+//                }else{
+//
+//                }
+//                DB::table('movies')
+//                    ->where('id',$movie->id)
+//                    ->update();
+//            }
+//        }
+//        // Get details of the movie
+//        $data = [];
+//        //$details =  Movie::find($movie);
+//        $links = $movie->movie_links()->where('quality_type','!=','screenshot')->get();
+//        $screenshots = $movie->movie_links()->where('quality_type','screenshot')->get();
+//        $data['links'] = $links;
+//        $data['screenshots'] = $screenshots;
+//
+//        $links = $movie->movie_links()->where('quality_type','!=','screenshot')->get();
+//        $screenshots = $movie->movie_links()->where('quality_type','screenshot')->get();
+//        $directors = $movie->crew()->where('profession','director')->get();
+//        $actors = $movie->crew()->where('profession','!=','director')->get();
+//        $data['details'] = $movie;
+//        $data['links'] = $links;
+//        $data['screenshots'] = $screenshots;
+//        $data['directors'] = $directors;
+//        $data['actors'] = $actors;
+//        $data['genre'] = $this->getCategories($movie->category);
+//        // ---------------- //
+//        //  Get related movies
+//        // TODO: FIX related movies categories
+//        $related_movies = Movie::take(7)->orWhere('category','like','%'.$movie->category.'%')
+//                                        ->where('id','!=',$movie->id)
+//                                        ->where('title','!=',$movie->title)
+//                                        ->orderBy('year','DESC')->get();
+//        $data['related'] = $related_movies;
+//
+//
+//        return view('movieDetails',compact('data'));
+        return $this->updateMovieFields('Dumb and Dumber To','2014');
     }
 
     /**
